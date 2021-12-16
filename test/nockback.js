041029288d51f9764bdcd2c4de131864477c85c5
Rob Jewell <jewell.robertp@gmail.com>
2021-12-16 14:54:30 -0500

    successfully use nock

--- test/nockback.js
+++ test/nockback.js
@@ -10,14 +10,20 @@ module.exports = function (fixtures) {
     beforeEach: function (done) {
       var filename = sanitize(this.currentTest.parent.title + this.currentTest.title + '.json');
 
+      console.log('filename', filename)
+      console.log('filenames', filenames)
+      console.log('filename indexOf', filenames.indexOf(filename) !== -1)
       // make sure we're not reusing the nock file
       if (filenames.indexOf(filename) !== -1) {
         return done(new Error('nock-back-mocha does not support multiple tests with the same name. ' + filename + ' cannot be reused.'));
       }
+
       filenames.push(filename);
 
       var previousFixtures = nock.back.fixtures;
+      console.log('previousFixtures', previousFixtures)
       nock.back.fixtures = fixtures;
+      console.log('fixtures', fixtures)
 
       nock.back.setMode('record')
 
